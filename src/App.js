8703724db56c328192e44e627abbe938082ff790
unknown <pottassiu@proton.me>
2024-03-15 18:00:02 -0300

    search bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Navbar from './components/nav/NavBar'
-
+import Footer from './components/footer/Footer'
 
 
 export default function App() {
   return (
     <div className="App">
       <Navbar />
+      <Footer />
     </div>
   );
 }
 
+                                                       
--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styles from './NavBar.module.css'
+import { FiSearch } from "react-icons/fi";
+
 
 const NavBar = () => {
     return (
@@ -9,10 +11,15 @@ const NavBar = () => {
             </div>
             <ul className={styles.wrapper} >
                 <li>Home</li>
-                <li>Suporte</li>
-                <li>Mais informações</li>
+                <li>Estabelecimentos</li>
+                <li>Produtos</li>
                 
             </ul>
+            <div className={styles.searchBar}>
+                <input type='text' name="search-est" placeholder="Procure..."/>
+                <FiSearch className={styles.searchIcon}/>
+                
+            </div>
             <ul className={styles.wrapperButtons}>
                 <button className={styles.btnCadastrar}>Cadastrar</button>
                 <button className={styles.btnEntrar}>Entrar</button>
